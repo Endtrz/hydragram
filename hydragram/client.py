47d25868030819cf3b71f69b2e500bae7c79bc10
𝗜𝒵𝗔𝗡𝗔 <halwakk07@gmail.com>
2025-07-13 02:37:43 +0545

    Initial commit

--- hydragram/client.py
+++ hydragram/client.py
@@ -1,22 +1,36 @@
 from pyrogram import Client as PyroClient
+from typing import Optional
 
 class Client:
-    _instance = None
-    app = None  # Global Pyrogram Client instance
+    _instance: Optional['Client'] = None
+    app: Optional[PyroClient] = None  # Global Pyrogram Client instance
 
-    def __init__(self, name, **kwargs):
+    def __init__(self, name: str, **kwargs):
         self._client = PyroClient(name, **kwargs)
         Client._instance = self
         Client.app = self._client  # Set global app to this instance
 
     @classmethod
-    def get_client(cls):
+    def get_client(cls) -> PyroClient:
         if cls._instance is None:
             raise RuntimeError("Client instance not created yet! Please create Client instance first.")
         return cls._instance._client
 
-    def run(self):
+    def run(self) -> None:
         self._client.run()
 
+    def add_handler(self, *args, **kwargs) -> None:
+        """Forward add_handler to the underlying Pyrogram client"""
+        self._client.add_handler(*args, **kwargs)
+
+    def on_message(self, filters=None, group: int = 0):
+        """Decorator to register message handlers"""
+        from pyrogram.handlers import MessageHandler
+        
+        def decorator(func):
+            self._client.add_handler(MessageHandler(func, filters), group)
+            return func
+        return decorator
+
 # Explicitly expose the app variable
-app = Client.app
+app: Optional[PyroClient] = Client.app
