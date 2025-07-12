42e42c95ebfb1b65526cf495133e81ce05d2a449
𝗜𝒵𝗔𝗡𝗔 <halwakk07@gmail.com>
2025-07-12 18:06:30 +0545

    Initial commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hydragram",
-    version="1.2",
+    version="1.2.1",
     author="Endtrz",
     author_email="endtrz@gmail.com",
     description="An enhanced Pyrogram-like filter and handler system using Kurigram.",
