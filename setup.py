b11c74238b421f1b84425772741a8fb57d024de2
Hasnain khan <161922960+hasnainkk-07@users.noreply.github.com>
2025-09-26 09:35:28 +0545

    ㅤㅤ

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hydragram",  # Your custom package name
-    version="3.1",  # Update this for each PyPI release
+    version="3.2.0",  # Update this for each PyPI release
     author="Endtrz",
     author_email="endtrz@gmail.com",
     description="An enhanced Pyrogram-like filter and handler system using Pyrogram.",
